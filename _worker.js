5962152479d99b77a75dde45584f25b774e3f423
yunfeic1990 <156538549+yunfeic1990@users.noreply.github.com>
2026-09-10 13:45:33 +0800

    更新 _worker.js

--- _worker.js
+++ _worker.js
@@ -1,4 +1,4 @@
-const Version = '2026-09-04 16:24:13';
+const Version = '2026-09-10
 let config_JSON, 缓存SOCKS5白名单 = null, 调试日志打印 = false;
 let SOCKS5白名单 = ['*tapecontent.net', '*cloudatacdn.com', '*loadshare.org', '*cdn-centaurus.com', 'scholar.google.com'];
 const Pages静态页面 = 'https://edt-pages.github.io';
